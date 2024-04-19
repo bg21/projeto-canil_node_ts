88b3133fde459d92466b2bb84cbf1fdcf6ba50a6
devbg21 <juhcosta23@gmail.com>
2024-04-18 23:04:19 -0300

    criando servidor

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import mustache from "mustache-express";
+import path from "path";
+
+dotenv.config();
+
+const server = express();
+server.set("view engine", "mustache");
+server.set("views", path.join(__dirname, "views"));
+server.engine("mustache", mustache());
+
+
+
+//definindo as pastas estáticas
+server.use(express.static(path.join(__dirname, "../public")));
+
+
+//rotas
+
+
+//se tentar todas as rotas e não der certo 
+server.use("", (req: Request, res: Response) => {
+    res.status(404).
+        render("erro");
+})
+
+//para a porta que o servidor irá abrir
+server.listen(process.env.PORT);
