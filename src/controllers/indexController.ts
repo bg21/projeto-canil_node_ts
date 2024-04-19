306b991b6efb5be1cc58d5d57a3fca7929fecb99
devbg21 <juhcosta23@gmail.com>
2024-04-18 23:37:02 -0300

    separando views

--- src/controllers/indexController.ts
+++ src/controllers/indexController.ts
@@ -4,11 +4,9 @@ export const index = (req: Request, res: Response) => {
     res.render("pages/index");
 }
 
-
 export const dogs = (req: Request, res: Response) => {
     res.render("pages/index");
 }
-
 
 export const cats = (req: Request, res: Response) => {
     res.render("pages/index");
