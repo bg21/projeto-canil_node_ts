73645a3a4a5b7451d3841b97172eec4edfd82184
devbg21 <juhcosta23@gmail.com>
2024-04-20 22:53:06 -0300

    menu ativo

--- src/controllers/indexController.ts
+++ src/controllers/indexController.ts
@@ -2,6 +2,11 @@ import { Request, Response } from "express";
 
 export const index = (req: Request, res: Response) => {
     res.render("pages/index", {
+        menu: {
+            all: true,
+            dog: false,
+            cat: false
+        },
         banner: {
             title: "Todos os animais",
             background: "allanimals.jpg"
@@ -10,9 +15,29 @@ export const index = (req: Request, res: Response) => {
 }
 
 export const dogs = (req: Request, res: Response) => {
-    res.render("pages/index");
+    res.render("pages/index", {
+        menu: {
+            all: false,
+            dog: true,
+            cat: false
+        },
+        banner: {
+            title: "Todos os cachorros",
+            background: "banner_dog.jpg"
+        }
+    });
 }
 
 export const cats = (req: Request, res: Response) => {
-    res.render("pages/index");
+    res.render("pages/index", {
+        menu: {
+            all: false,
+            dog: false,
+            cat: true
+        },
+        banner: {
+            title: "Todos os gatos",
+            background: "banner_cat.jpg"
+        }
+    });
 }
