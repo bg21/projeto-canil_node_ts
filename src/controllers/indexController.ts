a94aa6d62cda7768072f3ea5a37145705d48cfa5
devbg21 <juhcosta23@gmail.com>
2024-04-20 22:46:54 -0300

    deixando dinâmico

--- src/controllers/indexController.ts
+++ src/controllers/indexController.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from "express";
 
 export const index = (req: Request, res: Response) => {
-    res.render("pages/index");
+    res.render("pages/index", {
+        banner: {
+            title: "Todos os animais",
+            background: "allanimals.jpg"
+        }
+    });
 }
 
 export const dogs = (req: Request, res: Response) => {
